7ef6d192fbb4802194d6df51721a3139740a0cb0
Satyam Gupta <satyamgupta19946@gmail.com>
2018-04-30 00:47:07 +0530

    Update makedata.py
    Added a procedure to Extract Time also.

--- makedata.py
+++ makedata.py
@@ -1,7 +1,9 @@
-#Converts a Raw Whatsapp Group Chat Archived Txt into a Comma Separated Value
+#Converts a Raw Whatsapp Group Chat Archived Txt into a Comma Separated ValueError
 
 """"BEFORE RUNNING"""
 #1 - Convert your raw archived data to UTF-8 and a csv format. Can be done using notepad. file -> save as -> name.csv and change format to utf-8
+#2 - Open the file in excel and delete the entire column of Dates and shift the cells to the right. This is necessary because Whatsapp messages
+	 #containg enter are taken as new entries and thus the read_csv() function is not able to read them.
 #Change path file accordingly
 Pathoffile = r"C:\Users\Satyam\Desktop\Programs\Datasets\conversation\AlphaQ"
 
@@ -11,30 +13,35 @@ os.chdir(Pathoffile)
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
+import time
 
 #Reading Data and Making lists
 #Change filename accordingly
-filename = 'Alphaqv2.csv'
+filename = 'Alphaq.csv'
 chat = pd.read_csv(filename)
 chat = np.array(chat)
 Sender = list(['']*len(chat))
 Message = list(['']*len(chat))
+Time = list(([''])*len(chat))
 
 for i in range(len(chat)):
-	if ':' in chat[i][0]:
+	if ':' in chat[i][0]:  #This removes notifs like 'X' added 'y' in group.
 		newstr = chat[i][0][chat[i][0].index(':')+1:]
-	if	':' in newstr:
+	if ':' in newstr and 'M' in chat[i][0]:  #This removes notifs like 'X' changed the groups name, icon, left the group etc.
+		Time[i] = chat[i][0][1:chat[i][0].index('M')+1]
 		Sender[i] = newstr[:newstr.index(':')]
 		Message[i] = newstr[newstr.index(':')+2:]
 
+#Remove no Senders errors, newlines from messages, and entries with no Time.
 Sender = list(filter(None, Sender))
 Message = list(filter(None,Message))
+Time = list(filter(None,Time))
 
 for i in range(len(Sender)):
 	Sender[i] = Sender[i][8:]
 	
-# If an error of the array Sender and Message not being same length occurs. Remove the last Sender as it is probably a repeat of the last one.
+
 Sender.remove(Sender[-1])
-	
-mydata = pd.DataFrame({'Sender':Sender,'Message':Message})
-mydata.to_csv('my_data.csv')
+Time.remove(Time[-1])
+mydata = pd.DataFrame({'Time':Time, 'Sender':Sender,'Message':Message})
+# mydata.to_csv('my_data.csv')
